a4610db83970b5dbd686fda1d144fcdc80addc44
semdestroyer <semdestroyer@yandex.ru>
2021-01-14 02:20:35 +0200

    third commit

--- Main.py
+++ Main.py
@@ -45,4 +45,5 @@ def getPaste():
     return pasta
 #app.run( port=int(os.environ.get('PORT', 5000)))
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    app.run(port=int(os.environ.get('PORT', 5000)))
